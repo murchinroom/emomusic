dd2db6b7331e95f5aa86800ef9d50d83c310f6ac
CDFMLR <cdfmlr@163.com>
2023-05-12 15:34:02 +0800

    feat(api): add predicturi

--- emomusic/main.py
+++ emomusic/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
 from music_predict import MusicEmotionRecognition
 import tempfile
+import requests
 
 app = FastAPI()
 predictor = MusicEmotionRecognition()
@@ -29,3 +30,18 @@ async def predictfile(file: UploadFile) -> MusicEmotion:
 
         a, v = predictor.predict_data([tmp.name])
         return MusicEmotion(arouse=a[0], valence=v[0])
+
+# uri -> fetch -> predict
+@app.get("/predicturi")
+async def predicturi(mp3: str) -> MusicEmotion:
+    with tempfile.NamedTemporaryFile() as tmp:
+        # Download the MP3 file to the temporary file
+        response = requests.get(mp3, stream=True)
+        for chunk in response.iter_content(chunk_size=None):
+            tmp.write(chunk)
+        
+        # Predict emotion
+        a, v = predictor.predict_data([tmp.name])
+
+        return MusicEmotion(arouse=a[0], valence=v[0])
+
